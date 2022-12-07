48169f34991cbe1be531f0743ef5aa32b509c0ca
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2022-12-07 10:23:51 +0100

    day07 in C#

---
+++ day07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadLines("input.txt");
+var currentDirs = new Stack<string>();
+var dirSizes = new Dictionary<string, int>();
+foreach (var line in input) {
+    var match = Regex.Match(line, @"^\$ cd (.+)$");
+    if (match.Success) {
+        var newDir = Path.Combine(currentDirs.FirstOrDefault(""), match.Groups[1].Value);
+        if (newDir.EndsWith(".."))
+            currentDirs.Pop();
+        else {
+            currentDirs.Push(newDir);
+            dirSizes[newDir] = 0;
+        }
+        continue;
+    }
+
+    match = Regex.Match(line, @"^(\d+) (.+)$");
+    if (match.Success) {
+        var fileSize = int.Parse(match.Groups[1].Value);
+        foreach (var currentDir in currentDirs)
+            dirSizes[currentDir] += fileSize;
+    }
+}
+
+Console.WriteLine("C#");
+Console.WriteLine(Environment.GetEnvironmentVariable("part") == "part1"
+    ? dirSizes.Values.Where(x => x <= 100000).Sum()
+    : dirSizes.Values.Where(x => x >= 30000000 - (70000000 - dirSizes.First().Value)).Min());
