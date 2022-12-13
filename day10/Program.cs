11a43d5cdb75c7365b6b95cb095a6682c8f9a681
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2022-12-14 00:39:18 +0100

    day10 in C#

---
+++ day10/Program.cs
@@ -0,0 +1,24 @@
+﻿var cycle = 0;
+var cycleValue = new Dictionary<int, int> { [cycle] = 1 };
+foreach (var line in File.ReadLines("input.txt")) {
+    cycle++;
+    cycleValue[cycle] = cycleValue[cycle - 1];
+    if (line == "noop") continue;
+
+    var value = int.Parse(line.Split().Last());
+    cycle++;
+    cycleValue[cycle] = cycleValue[cycle - 1] + value;
+}
+
+Console.WriteLine("C#");
+
+if (Environment.GetEnvironmentVariable("part") == "part1") {
+    var cycles = new[] { 20, 60, 100, 140, 180, 220 };
+    Console.WriteLine(cycles.Select(c => c * cycleValue[c - 1]).Sum());
+    return;
+}
+
+for (var c = 0; c < cycle; c++) {
+    if (c % 40 == 0 && c > 0) Console.WriteLine();
+    Console.Write(new[] { cycleValue[c] - 1, cycleValue[c], cycleValue[c] + 1 }.Contains(c % 40) ? "#" : ".");
+}
