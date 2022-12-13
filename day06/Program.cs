26a279b5943037d5a8ab77496a5895d8d8b18de8
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2022-12-14 00:55:12 +0100

    day06 in C#

---
+++ day06/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadLines("input.txt").Single();
+
+Console.WriteLine("C#\n" + Environment.GetEnvironmentVariable("part") switch {
+    "part1" => Solution(4, input), "part2" => Solution(14, input), _ => string.Empty
+});
+
+static int Solution(int distinctChars, string input) {
+    for (var i = distinctChars; i < input.Length; i++) {
+        var slice = input[(i - distinctChars)..i];
+        if (slice.Distinct().Count() == distinctChars)
+            return i;
+    }
+    return -1;
+}
